72a38e02a84b250880e170a085b2ea04a0813262
stamander <iida.atsushi.fh@gmail.com>
2022-03-20 15:31:49 +0900

    api

--- basic-rest-api/app/app.js
+++ basic-rest-api/app/app.js
@@ -1,9 +1,47 @@
 const express = require('express')
-
 const app = express()
+const sqlite3 = require('sqlite3')
+const dbPath = "app/db/database.sqlite3"
 
-app.get('/api/v1/hello',(req,res)=>{
-  res.json({"message":"Hello World"})
+
+app.get('/api/v1/users',(req,res)=>{
+  //Conect database
+  const db = new sqlite3.Database(dbPath)
+
+  db.all("SELECT * FROM users",(err,rows) => {
+    res.json(rows)
+
+  })
+  db.close()
+
+})
+
+//get a user
+app.get('/api/v1/users:id',(req,res)=>{
+  //Conect database
+  const db = new sqlite3.Database(dbPath)
+  const id = req.params.id
+
+  db.get(`SELECT * FROM users WHERE id = ${id}`,(err,row) => {
+    res.json(row)
+
+  })
+  db.close()
+
+})
+
+//serch
+app.get('/api/v1/serch',(req,res)=>{
+  //Conect database
+  const db = new sqlite3.Database(dbPath)
+  const keyword = req.query.q
+
+  db.get(`SELECT * FROM users WHERE name LIKE "%${keyword}%"`,(err,row) => {
+    res.json(row)
+
+  })
+  db.close()
+
 })
 
 
