d344e84e51a98226883e0ffe33b003cefafea162
stamander <iida.atsushi.fh@gmail.com>
2022-03-20 18:00:07 +0900

    api

--- basic-rest-api/app/app.js
+++ basic-rest-api/app/app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 const sqlite3 = require('sqlite3')
+const  path  = require('path')
+
 const dbPath = "app/db/database.sqlite3"
 
+app.use(express.static(path.join(__dirname,'public')))
 
 app.get('/api/v1/users',(req,res)=>{
   //Conect database
@@ -30,19 +33,20 @@ app.get('/api/v1/users:id',(req,res)=>{
 
 })
 
-//serch
-app.get('/api/v1/serch',(req,res)=>{
+//search
+app.get('/api/v1/search',(req,res) => {
   //Conect database
   const db = new sqlite3.Database(dbPath)
   const keyword = req.query.q
 
-  db.get(`SELECT * FROM users WHERE name LIKE "%${keyword}%"`,(err,row) => {
-    res.json(row)
+  db.all(`SELECT * FROM users WHERE name LIKE "%${keyword}%"`,(err,rows) => {
+    res.json(rows)
 
   })
   db.close()
 
 })
+
 
 
 const port = process.env.PORT || 3000;
---
+++ basic-rest-api/app/public/js/index.js
@@ -0,0 +1,9 @@
+const indexModule=(()=>{
+  document.getElementById('search-btn').addEventListener('click', () => {
+    return searchModule.searchUsers()
+  })
+
+
+  return usersModule.fetchAllUsers()
+
+})()
